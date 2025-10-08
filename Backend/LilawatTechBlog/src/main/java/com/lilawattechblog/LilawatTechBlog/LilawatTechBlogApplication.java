94e2598a4db9b0946932f61f3978520656bcbe95
Mukesh Lilawat <143742837+mukeshlilawat1@users.noreply.github.com>
2025-10-08 16:32:35 +0530

    backend files upload

---
+++ Backend/LilawatTechBlog/src/main/java/com/lilawattechblog/LilawatTechBlog/LilawatTechBlogApplication.java
@@ -0,0 +1,13 @@
+package com.lilawattechblog.LilawatTechBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilawatTechBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilawatTechBlogApplication.class, args);
+	}
+
+}
